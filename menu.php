4d61f861f128f9edf3efd3e0fec12b60873122d5
Jocelyn Cheng <jac2367@barnard.edu>
2015-02-24 20:13:37 -0500

    initial commit

---
+++ menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE hmtl>
+<html>
+	<head>
+		<title>Jocelyn Cheng</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<header>
+		<figure>
+			<img src="images/headshot.jpg"/>
+		</figure>
+		<nav>
+			<ul>
+				<li><a href="index2.html">Home</a></li>
+				<li><a href="aboutme.html">About Me</a></li>
+				<li><a href="photos.html">Photo Gallery</a></li>
+			</ul>
+		</nav>
+	</header>
+	<body>
+		<footer>
+			<a href="https://www.facebook.com/joce.cheng">Facebook</a> |
+			<a href="https://instagram.com/joceyjocejoce/">Instagram</a> |
+			<a href="#">LinkedIn</a>
+		</footer>
+	</body>
+</html>
